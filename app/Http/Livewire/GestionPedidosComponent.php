94a307a1f3cdc1bccfc456e121ac05f17c5f0d93
domingors <domingors@gmail.com>
2021-02-24 13:41:13 +0000

    commit 24022021 1340

--- app/Http/Livewire/GestionPedidosComponent.php
+++ app/Http/Livewire/GestionPedidosComponent.php
@@ -2,12 +2,82 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\LPedido;
+use App\Models\Pedido;
+use App\Models\User;
 use Livewire\Component;
+use Illuminate\Support\Facades\Auth;
 
 class GestionPedidosComponent extends Component
 {
+    public $idUser;
+    public $idCabPed;
+    public $estado=2;
+    public $itemsPagina=0;
+    protected $usuarios, $cPedidos, $estados;
+
     public function render()
     {
-        return view('livewire.gestion-pedidos-component');
+        if($this->idUser==null)
+        $this->idUser=Auth::user()->id;
+
+        $this->usuarios=User::all();
+
+        $this->reloadPeds();
+        $users=$this->usuarios;
+        $estads=$this->estados;
+        $cPeds=$this->cPedidos;
+
+        return view('livewire.gestion-pedidos-component',compact('users','estads','cPeds'));
+    }
+    public function updatedIdUser()
+    {
+        $this->reloadPeds();
+        $users=$this->usuarios;
+        $estads=$this->estados;
+        $cPeds=$this->cPedidos;
+
+        return view('livewire.gestion-pedidos-component',compact('users','estads','cPeds'));
+    }
+    public function updatedEstado()
+    {
+        $this->reloadPeds();
+        $users=$this->usuarios;
+        $estads=$this->estados;
+        $cPeds=$this->cPedidos;
+
+        return view('livewire.gestion-pedidos-component',compact('users','estads','cPeds'));
+    }
+    public function reloadPeds(){
+        $this->estados=[[1,'Incompleto'],[2,'Pendiente'],[3,'Entregado']];
+
+        $this->cPedidos=Pedido::orderBy('id','desc')
+        ->where('user_id',$this->idUser)
+        ->Where('estado',$this->estado)
+        ->paginate();
+
+        if(!empty(($this->cPedidos)[0])){
+            if($this->idCabPed==null)
+            $this->idCabPed=($this->cPedidos)[0]->id;
+        }
+
+    }
+    public function putEstadoEntregado($id)
+    {
+        $ped=Pedido::find($id);
+        $ped->update([
+            'estado'=>3
+        ]);
+        return redirect('GestionPedidos');
+    }
+    public function pedToPdf($id)
+    {
+    }
+    public function destroy($id)
+    {
+        $Pedi=Pedido::find($id);
+
+        $Pedi->delete();
+        return redirect('GestionPedidos');
     }
 }
--- resources/views/livewire/gestion-pedidos-component.blade.php
+++ resources/views/livewire/gestion-pedidos-component.blade.php
@@ -1,3 +1,62 @@
 <div>
-    {{-- Because she competes with no one, no one can compete with her. --}}
+    <div class="py-12  flex items-center justify-between ">
+        <div class="max-w-3xl mx-auto">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg flex">
+                <div class="bg-white-500 rounded-lb shadow hoverflow-hiden p-4">
+                    <div class="form-group">
+                        <h2>Usuarios</h2>
+                        <select wire:model="idUser" wire: class="form-control" name="idUser" required >
+                            @foreach($users as $user)
+                                <option  value="{{$user->id}}"> {{$user->name}}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                </div>
+                <div class="bg-white-500 rounded-lb shadow overflow-hiden p-4">
+                    <div class="form-group">
+                        <h2>Estados</h2>
+                        <select wire:model="estado" wire: class="form-control" name="estado" required >
+                            @foreach($estads as $estad)
+                                <option  value="{{$estad[0]}}"> {{$estad[1]}}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container mx-auto">
+        <div class="bg-gray-50 px-4 py-3 border border-gray-200">
+            <div class="bg-white px-4 py-3 border border-gray-200">
+                <h2 align="center"><big>Pendiente de servir</big></h2>
+                <table class="min-w-full divide-y divide-gray-200" id="articulos">
+                    <thead class="bg-green-50 border-b">
+                        <tr>
+                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Id</th>
+                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Usuario</th>
+                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Estado</th>
+                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Acciones</th>
+                        </tr>
+                    </thead>
+                    <tbody class="bg-white divide-y divide-gray-200">
+                        @if($cPeds != null)
+                            @foreach ($cPeds as $cPed)
+                                <tr>
+                                    <td class="px-6 py-4 whitespace-nowrap">{{ $cPed->id }}</td>
+                                    <td class="px-6 py-4 whitespace-nowrap">{{ $cPed->user_id }}</td>
+                                    <td class="px-6 py-4 whitespace-nowrap">{{ $cPed->estado }}</td>
+                                    <td class="px-6 py-4 whitespace-nowrap">
+                                        <a href="#" type="button" wire:click="putEstadoEntregado({{ $cPed->id }})" class="bg-green-300 hover:gb-green-700 rounded">Marcar entregado</a>
+                                        <a href="#" type="button" wire:click='destroy({{ $cPed->id }})' class="bg-blue-300 hover:gb-blue-700 rounded">Generar pdf</a>
+                                        <a href="#" type="button" wire:click='destroy({{ $cPed->id }})' class="bg-red-300 hover:gb-red-700 rounded">Borrar</a>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        @endif
+                    </tbody>
+                </table>
+                {{ $cPeds->links() }}
+            </div>
+        </div>
+    </div>
 </div>
